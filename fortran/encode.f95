a2789c7bd60a7ce82d6272d130e00b6de8026693
dcods22 <daniel.cody2@marist.edu>
2014-01-28 19:08:58 -0500

    PLayed with prints, strings, chars, loops

--- fortran/encode.f95
+++ fortran/encode.f95
@@ -1,13 +1,37 @@
 program first
 !comments
 
-CHARACTER(LEN=20) :: testString="Hello World"
+CHARACTER(LEN=20) :: testString="hello world"
 
-print *,testString
+INTEGER y
 
-subroutine upperCase(x)
+CHARACTER(LEN = 1) c
 
-return
-end
+do n=1, 20, 1
+
+c = testString(n:n+1)
+y = iachar(c)
+y = y - 32
+c = achar(y)
+
+print *,c
+
+end do
+
+
 
 end program first
+
+subroutine upperCase(x)
+   CHARACTER(LEN = *) x
+   INTEGER charValue 
+   
+   
+   do n=1, 20, 1
+
+	!charValue = iachar(letter)
+   
+   end do
+
+end subroutine
+
