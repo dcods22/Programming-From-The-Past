cf6c9ba8481c07b23d26e178a6bb11b502bb88c7
dcods22 <daniel.cody2@marist.edu>
2014-02-03 22:15:55 -0500

    Added decode and solve functionality

--- pascal/encode.pas
+++ pascal/encode.pas
@@ -1,11 +1,13 @@
 Program Encode;
 
 (****** Variables ********)
-var str: String;
+var EStr: String;
+var SStr: String;
+var DStr: String;
 var newStr: String;
-var a : integer;
+var i : integer;
+var n : integer;
 var chInt : LongInt;
-var moveAmt : integer;
 var ch : String;
 
 (******* Functions ******)
@@ -15,21 +17,107 @@ function upper(str: String):String;
 var
    result: String;
 begin
-    for a := 1  to Length(str) do
+    ch := '';
+    chInt := 0;
+    result := '';
+    for i := 1  to Length(str) do
     begin
-        chInt := ord(str[a]);
-        chInt := chInt - 32;
+        chInt := ord(str[i]);
+        if( chInt > 90) then
+        begin
+            chInt := chInt - 32;
+        end;
         ch := chr(chInt);
-       result :=result + ch;
+        result :=result + ch;
     end; 
     upper := result
 end;
 
+function encode(str: String; moveAmt: integer):String;
+var
+    result: String;
+begin
+    ch := '';
+    chInt := 0;
+    result := '';
+    for i :=1 to Length(str) do
+    begin
+        chInt := ord(str[i]);
+        if (chInt > 64) then
+        begin
+            chInt := chInt + moveAmt;
+            if chInt > 90 then
+            begin
+                chInt := 64 + (chInt - 90);
+            end
+        end;
+        ch := chr(chInt);
+        result := concat(result, ch);
+    end;    
+    encode := result
+end;
 
+function decode(str: String; moveAmt: integer):String;
+var
+    result: String;
+begin
+    ch := '';
+    chInt := 0;
+    result := '';
+    for i :=1 to Length(str) do
+    begin
+        chInt := ord(str[i]);
+        if (chInt > 64) then
+        begin
+            chInt := chInt - moveAmt;
+            if (chInt < 64) then
+            begin
+                chInt := 90 - (64 - chInt);
+            end;
+        end; 
+        ch := chr(chInt);
+        result :=result + ch;
+    end;    
+    decode := result
+end;
+
+function solve(str: String; solves: integer):String;
+var
+    result: String;
+begin
+    ch := '';
+    chInt := 0;
+    result := '';
+    for n := solves downto 0 do
+    begin
+        result :='';
+        for i :=1 to Length(str) do
+        begin
+            chInt := ord(str[i]);
+            if (chInt > 64) then
+            begin
+                chInt := chInt + n;
+                if chInt > 90 then
+                begin
+                    chInt := 64 + (chInt - 90);
+                end
+            end;
+            ch := chr(chInt);
+            result := concat(result, ch);
+        end;    
+        writeln('Caeser ', n, ':', result);
+    end;
+end;
 (******* Main *****)
 begin
-    moveAmt := 1;
-    str := 'test';
-    str := upper(str);
-    writeln(str);
+    EStr := 'this iS A tEst STRing from dan222';
+    DStr := 'BPQA Qa I BmaB abzQVO NZWU LIV222';
+    SStr := 'hal';
+    EStr := upper(EStr);
+    DStr := upper(DStr);
+    SStr := upper(SStr);
+    
+    writeln(encode(EStr, 8));
+    writeln(decode(DStr, 8));
+    solve(SStr, 26);
 end.
