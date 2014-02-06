18db5d5ec07d8fb331138007f1e9071505dac282
dcods22 <daniel.cody2@marist.edu>
2014-02-06 16:40:37 -0500

    String functions

--- scala/encode.scala
+++ scala/encode.scala
@@ -1,10 +1,26 @@
 object encode {
-	var str : String = "this is a test string";
 	def main(args: Array[String]) {
-		var i = 0;
+		var str : String = "this is a test string";
+		var newStr : String = "";
+		var Estr : String = null;
+		var Dstr : String = null;
+		var i : Int = 0;
+		var moveAmt : Int = 1;
+		var charInt : Int = 0;
+		var char : Char = 'a'; 
 		str = str.map(_.toUpper);
-		for( i <- 0 to 10 ){
-			println(str.substring(i,i+1));	
+		for( i <- 0 to str.size - 1){
+			char = str.charAt(i);
+			charInt = char.toInt;
+			if( charInt > 64 ){
+				charInt = charInt + moveAmt;
+			}
+			if( charInt > 90) {
+				charInt = 64 + (charInt - 90);
+			}
+			char = charInt.toChar;
+			newStr = newStr + char;
 		}	
+		print(newStr);
 	}
 }
